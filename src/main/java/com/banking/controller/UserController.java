4b5b0a241f3d4e34854ce00693a776fc1bf7516f
malavika <malavika13r@gmail.com>
2024-02-22 11:30:07 +0000

    no user exception handling

--- src/main/java/com/banking/controller/UserController.java
+++ src/main/java/com/banking/controller/UserController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banking.entity.User;
+import com.banking.exceptions.NoSuchUserException;
 import com.banking.service.UserService;
+
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +26,13 @@ public class UserController {
 	UserService userService;
 	
 	@GetMapping("/user/{id}")
-	public User getUser(@PathVariable int id) {
-		return userService.getUserById(id);
+	public User getUser(@PathVariable int id) throws NoSuchUserException {
+		try {
+			return userService.getUserById(id);
+		}
+		catch(NoSuchElementException ex) {
+			throw new NoSuchUserException("No such user present in the system");
+		}
 	}
 	
 	@PutMapping("/updateUser")
